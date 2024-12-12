b8275cc981392239ac1b9c246258ec524ec95468
Your Name <tkhan8312@gmail.com>
2024-12-12 16:37:43 +0530

    all files push to git

---
+++ Mern stack/Javascript/index.js
@@ -0,0 +1,237 @@
+// console.log("I am Tanveer Khan")
+// console.log("i am coder")
+// let name="Tanveer khan"
+// console.log("name");
+// console.log(typeof(studentname));
+// let marks= 10;
+// console.log(typeof(marks));
+// console.log("Tanveer KHan");
+// console.log(5+5);
+// let freinds =("udhhd,hdsh,iusdhjhdg");
+// console.log("friands");
+// console.log(typeof("friends"));
+// let fight= false;
+// console.log(typeof(fight));
+// var myname= "Tanveer khan";
+// console.log(myname)
+// const myname1 = "Tanveer khan";
+// console.log(myname1)
+// let school= "vidyanikatan";
+// console.log(school)
+// school = "st.joseph"
+// console.log(school)
+// let add= (56546+5654564);
+// console.log(add)
+// console.log(5==5);
+// console.log('7'===7);
+// let age = 18;
+// if (age= 18){
+//     console.log("voting");
+// }else{
+//     console.log("not voting");
+// }
+// let balance = 100000000;
+// if (balance===1000){
+//     console.log("poor");
+// }else if(balance=== 5000){
+//     console.log("poor");
+// }else if (balance===100000){
+//     console.log("middle class");
+// }else{
+//     console.log("rich");
+// // }
+// console.log(true|| flase);
+// console.log(true && false);
+// console.log(true||true||true);
+// console.log(true&&false&&true);
+
+// And oprator
+// let apple = true;
+// let mango = true;
+// console.log( apple && mango);
+
+// or operator
+// let apple= false;
+// let mango= false;
+// console.log( apple || mango);
+
+// not operator
+
+// For loop example
+// for{let i=1 ; i<=10 ; ++i}
+// {
+//     console.log(i);
+// }
+//  for (let i = 1 ; i<= 10 ; ++i){
+//     console .log(i);
+//  }
+
+//  for (let i = 1 ; i<= 5 ; ++i){
+//     console .log(i);
+//  }
+
+//  for (let i = 2 ; i<= 10 ; i+=2 ){
+//     console .log(i);
+//  }
+
+//  for (let i = 3 ; i<= 30 ; i+=3 ){
+//     console .log(i);
+//  }
+
+// for (let i = 5 ; i<= 50 ; i+=5 ){
+//     console .log(i);
+//  }
+
+//  for (let i = 1 ; i< 50 ; i+=2 ){
+//     console .log(i);
+//  }
+
+//  for (let i = 1 ; i< 50 ; i+=2 ){
+//     console .log("digit:"+i);
+//  }
+
+// for (let i = 1 ; i<=10 ; i++ ){
+//     if (i===9)continue;
+//     console .log(i);
+
+// for (let i = 1 ; i<=10; ++i){
+//     console.log(i + (i*10))
+// }
+
+// for (let i = 2 ; i<=10; i+=2){
+
+    // for (let i = 5 ; i>=1; --i)
+    //     {
+    //     console.log(i);
+    // }
+    // for(let a=7 ; a<=1;--a){
+    //     console.log(a);
+    // }
+
+    // for(let b=10 ;b>=1 ; --b){
+    //     console.log(b);
+    // }
+    
+    // console.log(" ");
+
+    // for(let b=7 ;b>=2 ; --b){
+    //     console.log(b);
+    // }
+
+    // let i = 1;
+    // while( i<=10){
+    //     console.log(i);
+    //     ++i;
+    // }
+    //  let a = 10;
+    //  while(a>=3){
+    //     console.log(a);
+    //     --a;
+    //  }
+     
+    // if and else
+
+    // score = 80
+    // if(score ===79){
+    //     console.log("grad c")
+    // }else if( score === 70){
+    //     console.log("grad d")
+    // }else if(score >79){
+    //     console.log("grad a")
+    // }
+
+    // age = 18
+    // if (age===10){
+    //     console.log("not eligible for vote")
+    // }else if ( age>15){
+    //     console.log("eligible for vote india")
+    // }
+
+    // for loop with continue
+
+    // for ( let a= 1 ; a <=10 ; ++a){
+    //     if(a % 4===0){
+    //         continue;
+
+    //     }
+    //    console.log(a);
+
+    // }
+
+    // console.log(" ")
+
+    // for ( let a= 10 ; a >=1 ; --a){
+    //     if(a % 3===0){
+    //         continue;
+
+    //     }
+    //    console.log(a);
+
+    // }
+    
+    // let age = 15
+    // if (age <18){
+    //     console.log("not eligible for vote");
+    // }else if(age===10){
+    //   console.log("not eligible for vote"); 
+    // }else if(age>=18){
+    //     console.log("you are eligible for vote");
+    // }else{
+    //     console.log("not vote");
+    // }
+
+    // for(let x = 1; x<=10 ; ++x){
+    //     console.log(x);
+    // }
+
+    // for ( let a = 2 ; a <=20 ; a+=2){
+    //     console.log(a);
+    // }
+    // console.log(" space");
+    // for ( let b = 3 ; b <=30 ; b+=3){
+    //     console.log(b);
+    // }
+
+    // for (let x= 1 ; x<=10 ; ++x){
+    //     console.log( x + (2*3));
+    // }
+
+    // let a = 1;
+    // while(a<=20){
+    //     console.log("*");
+    //     a+=2;
+
+    // }
+    // function num (a,b){
+    //     console.log(a+b);
+    // }
+    // num (1,3);
+
+    // function multiply ( x,y){
+    //     console.log(x*y);
+    // }
+    // multiply (5,6);
+     
+    // function add(a,b){
+    //     return a + b;
+    // }
+    // let addmubers=add(5,3);
+    // console.log(addmubers);
+
+    // const numbers=(a,b)=>(a*b);
+    // let result = numbers(3,5);
+    // console.log( result);
+
+    // const add = (a,b)=> (a%b)
+
+// a=100
+// b= 200
+// console.log(a>=b);
+
+// let name = "Tanveer Khan";
+// if (name= "Tanveer khan"){
+//     console.log("name is correct");
+// }else{
+//     console.log("name is incorrect");
+// }
+
